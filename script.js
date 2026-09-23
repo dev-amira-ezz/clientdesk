d9161b2eb65cc7fe78deac148b8d6c499ca09223
Amira Ezz <aezz4007@gmail.com>
2026-09-23 10:57:03 +0300

    Create basic HTML and add customer data

---
+++ script.js
@@ -0,0 +1,8 @@
+const customers = [
+    { name: "Alice", country: "USA", plan: "pro", fee: 25 },
+    { name: "John", country: "UK", plan: "free", fee: 0 },
+    { name: "Maria", country: "USA", plan: "pro", fee: 25 },
+    { name: "Ahmed", country: "Egypt", plan: "pro", fee: 15 },
+    { name: "Sara", country: "Egypt", plan: "free", fee: 0 }
+];
+
