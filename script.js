c169815c609b66ee5aa32f1d2917f3df4892f4b0
Amira Ezz <aezz4007@gmail.com>
2026-09-24 12:37:00 +0300

    Display customer table

--- script.js
+++ script.js
@@ -6,3 +6,37 @@ const customers = [
     { name: "Sara", country: "Egypt", plan: "free", fee: 0 }
 ];
 
+const showCustomerList = () => {
+    // I create the table head since it's stable
+    const customersDiv = document.getElementById('customers');
+    const customersTable = document.createElement('table');
+    customersDiv.appendChild(customersTable);
+    customersTable.innerHTML += `
+<caption>Customer List</caption>
+<thead>
+<tr>
+<th scope="col">Name</th>
+<th scope="col">Country</th>
+<th scope="col">Plan</th>
+<th scope="col">Monthly Fee</th>
+</tr>
+</thead>
+`
+// I create the table body and attach the customer data to it using forEach
+    const customersTableBody = document.createElement('tbody');
+    customersTable.appendChild(customersTableBody);
+   
+   
+    customers.forEach(customer => {
+        customersTableBody.innerHTML += `
+    <tr>
+    <td>${customer.name}</td>
+    <td>${customer.country}</td>
+    <td>${customer.plan.charAt(0).toUpperCase() + customer.plan.slice(1)}</td>
+    <td>$${customer.fee}</td>
+    </tr>
+`
+    });
+}
+
+document.onload = showCustomerList();
